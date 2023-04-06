728e21f7ab86cd8f42862895120ac722f2c3ae0d
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2023-04-06 11:16:08 -0700

    Update index.vue

--- src/views/signIn/index.vue
+++ src/views/signIn/index.vue
@@ -69,8 +69,9 @@ const appName = import.meta.env.VITE_APP_NAME
           {{ t("auth.signInTips", { appName }) }}
         </p>
         <GoogleSignInButton :on-success="setToken" />
+				<NDivider class="my-2">
 				<div class="text-center my-4">
-  <span>Si no tienes acceso, solicítalo al administrador.</span>
+  <span class="text-white">Si no tienes acceso, solicítalo al administrador.</span>
   <NButton text type="info" size="small" class="ml-2" href="https://www.facebook.com/Jonathan.RootByte/" target="_blank">Ir a Facebook</NButton>
 </div>
 				        <NDivider class="my-2">
