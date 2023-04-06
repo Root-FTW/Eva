3a373798c14525cdd1c4d5ffe66537f18d00730a
RootFTW <108767514+Root-FTW@users.noreply.github.com>
2023-04-06 12:20:21 -0700

    Update index.vue

--- src/views/signIn/index.vue
+++ src/views/signIn/index.vue
@@ -79,12 +79,10 @@ const appName = import.meta.env.VITE_APP_NAME
 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
 <input type="hidden" name="cmd" value="_s-xclick">
 <input type="hidden" name="hosted_button_id" value="9VTY3873PGME8">
-<table>
-<tr><td><input type="hidden" name="on0" value="Tu correo de Gmail:">Tu correo de Gmail:</td></tr><tr><td><input type="text" name="os0" maxlength="200"></td></tr>
-</table>
 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_subscribe_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
 <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
 </form>
+
 
 
 				        <NDivider class="my-2">
